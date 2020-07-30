224d7dd1470c4008830f5e54accc99c290979552
liamhaller <38738609+liamhaller@users.noreply.github.com>
2020-07-29 22:44:32 -0400

    Update app.R

--- passive_portfolio/app.R
+++ passive_portfolio/app.R
@@ -15,7 +15,7 @@ library(shinydashboard)
 library(shinyjs)
 library(htmltools)
 library(bsplus)
-source("/shiny-serve/passive_portfolio/helper.R")
+source("/shiny-server/passive_portfolio/helper.R")
 
 
 #TODO
